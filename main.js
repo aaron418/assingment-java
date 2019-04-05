6a88ec4541924d415acb12c0eb5f96d9b265caab
aaron wurie <aaronw0418@gmail.com>
2019-04-05 10:45:36 -0400

    new folder

---
+++ main.js
@@ -0,0 +1,25 @@
+
+// function printInt(n){
+//     for(var i=1;i<=n;i++){
+//         console.log(i);
+
+//     }
+// }
+
+// // printInt(18);
+
+// function printIntRev(n){
+//      for(var x=n;x>=1;x--){
+//         console.log(x);
+//      }
+// }
+
+//     printIntRev(5)
+
+
+// Question 3
+function checkInput(x) {
+    
+}
+
+console.log(checkInput("ki"));
