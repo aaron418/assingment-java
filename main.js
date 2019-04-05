246896d2f13f7d0bf4ed93322b0a301ef3434b21
aaron wurie <aaronw0418@gmail.com>
2019-04-05 15:43:14 -0400

    done at school saving for home

--- main.js
+++ main.js
@@ -1,25 +1,79 @@
 
-// function printInt(n){
-//     for(var i=1;i<=n;i++){
-//         console.log(i);
+function printInt(n){
+    for(var i=1;i<=n;i++){
+        console.log(i);
 
-//     }
-// }
+    }
+}
 
-// // printInt(18);
+// printInt(18);
 
-// function printIntRev(n){
-//      for(var x=n;x>=1;x--){
-//         console.log(x);
-//      }
-// }
+function printIntRev(n){
+     for(var x=n;x>=1;x--){
+        console.log(x);
+     }
+}
 
 //     printIntRev(5)
 
 
 // Question 3
-function checkInput(x) {
+// function checkInput(x) {
+//     var x = 5;
     
+//     if (x > -1) {
+//       console.log(checkInput(x))("x is greater than -1 returns number");
+//     } else {
+//       console.log(checkInput(x))("x is less than or equal to -1 we return -1");
+//     } 
+
+
+//     var x = 'hello';
+    
+//     if (x > -1) {
+//         console.log(checkInput(x))("x is greater than -1 returns number");
+//       } else {
+//         console.log(checkInput(x))("x is less than or equal to -1 we return -1");
+//       } 
+
+//       var x = '5'
+
+//       if (x > -1) {
+//         console.log(checkInput(x))("x is greater than -1 returns number");
+//       } else {
+//         console.log(checkInput(x))("x is less than or equal to -1 we return -1");
+//       } 
+
+//       var x = true
+
+//       if (x > -1) {
+//         console.log(checkInput(x))("x is greater than -1 returns number");
+//       } else {
+//         console.log(checkInput(x))("x is less than or equal to -1 we return -1");
+//       } 
+
+//       var x = ([1,2,3,4])
+
+//       if (x > -1) {
+//         console.log(checkInput(x))("x is greater than -1 returns number");
+//       } else {
+//         console.log(checkInput(x))("x is less than or equal to -1 we return -1");
+//       } 
+
+// }
+
+// console.log(checkInput("null"));
+
+
+// question 4
+function simpleEvenAdding(num){
+    var solution = 0;
+    for (var x = 0; x <= num; x++){ 
+        if(x % 2 === 0){
+            solution += x; // solution = solution + x
+        }
+    }
+    return solution;
 }
 
-console.log(checkInput("ki"));
+console.log(simpleEvenAdding(0))
